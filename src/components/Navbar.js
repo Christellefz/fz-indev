f892b8b6b4029faa03ac0deb8422d8453cc1deda
Christellefz <christellefz@yahoo.fr>
2021-12-15 13:34:19 +0100

    burger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,13 @@ import { FaFacebookF, FaInstagram, FaLinkedin } from 'react-icons/fa'
 
 import Logo from './assets/images/Logo.png'
 import { links } from '../data'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false)
+  const handleShowlinks = () => {
+    setShowLinks(!showLinks)
+  }
   const handleClick = e => {
     e.preventDefault()
     const target = e.target.getAttribute('href')
@@ -17,55 +22,58 @@ const Navbar = () => {
     })
   }
   return (
-    <div className='nav-container'>
+    <div className='nav-container '>
       <div className='nav-contact'>
         <span className='nav-contact-item'>Tel: 06 66 36 42 27</span>
         <span className='nav-contact-item'>Mail: contact@fzindev.fr</span>
       </div>
-      <div className='nav-core'>
-        <img className='logo' alt='logo de fz indev' src={Logo} />
-
+      <div className={`nav-core ${showLinks ? 'show-nav' : 'hidden-nav'}`}>
         <div className='navAndSocial'>
-          <nav className='navItems'>
+          <nav className='navItems '>
             {links.map(link => {
               return (
                 <a
                   href={link.url}
                   key={link.id}
                   onClick={handleClick}
-                  className='link'
+                  className='link , slideInDown'
                 >
                   {link.text}
                 </a>
               )
             })}
+
+            <div className='social-Icons-block'>
+              <a
+                href='https://www.facebook.com/christelle.fernandez.73/'
+                title='facebook fz-indev'
+                target='blank'
+                className='socialMediaIcon'
+              >
+                <FaFacebookF size={24} />
+              </a>
+              <a
+                href='https://www.facebook.com/christelle.fernandez.73/'
+                title='facebook fz-indev'
+                target='blank'
+                className='socialMediaIcon'
+              >
+                <FaInstagram size={24} />
+              </a>
+              <a
+                href='https://www.facebook.com/christelle.fernandez.73/'
+                title='facebook fz-indev'
+                target='blank'
+                className='socialMediaIcon'
+              >
+                <FaLinkedin size={24} />
+              </a>
+            </div>
+            <img className='logo' alt='logo de fz indev' src={Logo} />
           </nav>
-          <div className='social-Icons-block'>
-            <a
-              href='https://www.facebook.com/christelle.fernandez.73/'
-              title='facebook fz-indev'
-              target='blank'
-              className='socialMediaIcon'
-            >
-              <FaFacebookF size={24} />
-            </a>
-            <a
-              href='https://www.facebook.com/christelle.fernandez.73/'
-              title='facebook fz-indev'
-              target='blank'
-              className='socialMediaIcon'
-            >
-              <FaInstagram size={24} />
-            </a>
-            <a
-              href='https://www.facebook.com/christelle.fernandez.73/'
-              title='facebook fz-indev'
-              target='blank'
-              className='socialMediaIcon'
-            >
-              <FaLinkedin size={24} />
-            </a>
-          </div>
+          <button className='navbar-burger' onClick={handleShowlinks}>
+            <span className='burger-bar'></span>
+          </button>
         </div>
       </div>
     </div>
